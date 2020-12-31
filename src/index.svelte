a5eb93ce96dfc1f9b07f83fc4602bd806a8b5717
Mekyle <mekylenaidoo@yahoo.com>
2020-12-31 16:15:53 +0200

    Initial commit

---
+++ src/index.svelte
@@ -0,0 +1,34 @@
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900&display=swap" rel="stylesheet">
+
+<main class="bg-gray-900 font-montserrat">
+    <header class="h-24 sm:h-32 flex items-center">
+        <div class="font-black text-purple-500 text-2xl pl-6 sm:pl-20 flex">
+                Eth<span class="text-white">Grounds</span>
+            </div>
+        <div class="container mx-auto px-4 sm:px-6 flex items-center justify-between">
+            <div class="flex items-center">
+                <nav class="text-white md:text-lg text-md flex items-center">
+                    <a href="https://t.me/ethgrounds" class="py-2 px-3 flex hover:text-purple-500 text-green-300">
+                        Telegram
+                    </a>
+                    <a href="https://discord.gg/znrEDDQca8" class="py-2 px-3 flex hover:text-purple-500 text-green-300">
+                        Discord
+                    </a>
+                    <a href="https://twitter.com/EthGrounds" class="py-2 px-3 flex hover:text-purple-500 text-green-300">
+                        Twitter
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <div class="container mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row relative">
+        <div class="sm:w-6/12 relative z-10">
+            <img class="w-full" src="/santa.png" />
+        </div>
+        <div class="sm:w-5/12 xl:w-4/12 flex flex-col items-start sm:items-end sm:text-right ml-auto mt-8 sm:mt-0 relative z-10 xl:pt-20 mb-16 sm:mb-0">
+            <h1 class="text-4xl lg:text-5xl text-white leading-none mb-4 font-black">Be like <span class="text-red-500">Santa</span> and live your dreams with <span class="text-purple-500">Eth</span>Grounds</h1>
+            <p class="lg:text-lg mb-4 sm:mb-12 text-white"><span class="text-purple-500">Eth</span>Grounds allows you to invest in passion projects that traditional capital never would. As a project creator you can accept capital from anyone and anywhere securely through our smart contracts.</p>
+            <a href="#" class="font-semibold text-lg bg-green-600 hover:bg-white hover:text-black text-white py-3 px-12 rounded-full">Learn more</a>
+        </div>
+    </div>
+</main>
